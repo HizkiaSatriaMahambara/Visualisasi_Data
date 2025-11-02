58eb4d914769486a31b658e98e3b5c2cd1b74812
HizkiaSatriaMahambara <hizatma@gmail.com>
2025-11-02 19:28:49 +0700

    Update UTS_VisualData.py

--- uts/UTS_VisualData.py
+++ uts/UTS_VisualData.py
@@ -6,7 +6,8 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="InsightBoard - Analisis Penjualan", layout="wide", initial_sidebar_state="expanded")
 
-CSV_PATH = "D:/Visual data/uts/Copy of finalProj_df - df.csv"
+CSV_PATH = "CSV_PATH = "uts/Copy of finalProj_df - df.csv"
+"
 df = pd.read_csv(CSV_PATH)
 
 date_cols = [c for c in df.columns if "date" in c.lower() or "time" in c.lower()]
@@ -142,3 +143,4 @@ elif menu == "Segmentasi Pelanggan":
         st.dataframe(df_seg.head(15))
     else:
         st.warning("Kolom customer_id tidak ditemukan dalam dataset.")
+
